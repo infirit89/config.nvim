c57423cfd77de2068b557fd798e9489f9fa39788
infirit89 <georgedautov808@gmail.com>
2025-05-10 00:57:37 +0300

    added editorconfig and oil.nvim

--- init.lua
+++ init.lua
@@ -683,7 +683,13 @@ require('lazy').setup({
         --
         -- But for many setups, the LSP (`tsserver`) will work just fine
         ts_ls = {},
-        csharp_ls = {},
+        csharp_ls = {
+          settings = {
+            completion = {
+              importOnCompletion = true,
+            },
+          },
+        },
         clangd = {},
         zls = {},
         rust_analyzer = {},
@@ -986,6 +992,7 @@ require('lazy').setup({
   require 'kickstart.plugins.autopairs',
   require 'kickstart.plugins.neo-tree',
   require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
+  require 'custom.plugins.oil-nvim',
 
   -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
   --    This is the easiest way to modularize your config.
---
+++ lua/custom/plugins/oil-nvim.lua
@@ -0,0 +1,10 @@
+return {
+  'stevearc/oil.nvim',
+  dependencies = {
+    'nvim-tree/nvim-web-devicons',
+  },
+  opts = {},
+  keys = {
+    { '<leader>o', '<cmd>Oil<cr>', desc = 'Oil' },
+  },
+}
